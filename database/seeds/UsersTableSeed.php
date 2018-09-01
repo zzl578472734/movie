a00652e8f5b3b82b2caede3824adefd3b0a1b10d
zhengzhilun <18459270924@163.com>
2018-09-01 23:48:51 +0800

    添加了数据迁移文件

---
+++ database/seeds/UsersTableSeed.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeed extends Seeder
+{
+    /**
+     * Run the database seeds.
+     * 批量生成用户数据
+     * @return void
+     */
+    public function run()
+    {
+        $users = factory(\App\Models\User::class)->times(50)->make();
+        \App\Models\User::query()->insert($users->toArray());
+    }
+}
